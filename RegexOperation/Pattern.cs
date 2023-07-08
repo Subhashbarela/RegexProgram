f62780c84ece3094d6dff40995a60dc113da337c
Subhash Barela <barelasubhash16@gmail.com>
2023-07-08 11:20:57 +0530

    [subhash] find the sequence of lower case char

--- RegexOperation/Pattern.cs
+++ RegexOperation/Pattern.cs
@@ -9,11 +9,34 @@ namespace RegexOperation
 {
     public class Pattern
     {
-        public static string pattern = "^ab{2,3}";
-        public bool CharMatch(string message)
+        public void CharMatch(string message)
         {
+            string pattern = "^ab{2,3}";
+
             bool match = Regex.IsMatch(message, pattern);
-            return match;
+             
+            if (match)
+            {
+                Console.WriteLine("String is valid...!");
+            }
+            else
+            {
+                Console.WriteLine("String is not valid...!");
+            }         
+        }
+        public void SequenceCharMatch(string message)
+        {     
+            string pattern = "^[a-z]+_[a-z]+$";
+
+        bool match = Regex.IsMatch(message, pattern);
+            if (match)
+            {
+                Console.WriteLine("String is valid...!");
+            }
+            else
+            {
+                Console.WriteLine("String is not valid...!");
+            }
         }
     }
 }
--- RegexOperation/Program.cs
+++ RegexOperation/Program.cs
@@ -11,17 +11,37 @@ namespace RegexOperation
         static void Main(string[] args)
         {
             Pattern pattern = new Pattern();
-            Console.WriteLine("Enter the string : ");
-            string str = Console.ReadLine();
-            bool isValid = pattern.CharMatch(str);
-            if (isValid)
-            {
-                Console.WriteLine("String is valid...!");
-            }
-            else
-            {
-                Console.WriteLine("String is not valid...!");
-            }
+            string ans = "";
+            do {
+                Console.WriteLine("1:Charctor Checking \n2: Sequence Of Char Checking");
+                Console.WriteLine("Enter the choice");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter the string : ");
+                            string str = Console.ReadLine();
+                            pattern.CharMatch(str);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter the string : ");
+                            string str = Console.ReadLine();
+                            pattern.CharMatch(str);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please insert the valid number");
+                            break;
+                        }
+
+                }
+                Console.WriteLine("Do you want to continue..?");
+                ans=Console.ReadLine();
+            } while (ans=="yes" || ans=="y");
             Console.ReadLine();
         }
     }
