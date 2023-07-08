747a4f981873703898190e6544d5c11b1d4bb342
Subhash Barela <barelasubhash16@gmail.com>
2023-07-08 11:13:10 +0530

    [Subhash] Check given char is valid or not

---
+++ RegexOperation/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexOperation
+{
+    public class Pattern
+    {
+        public static string pattern = "^ab{2,3}";
+        public bool CharMatch(string message)
+        {
+            bool match = Regex.IsMatch(message, pattern);
+            return match;
+        }
+    }
+}
---
+++ RegexOperation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter the string : ");
+            string str = Console.ReadLine();
+            bool isValid = pattern.CharMatch(str);
+            if (isValid)
+            {
+                Console.WriteLine("String is valid...!");
+            }
+            else
+            {
+                Console.WriteLine("String is not valid...!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
